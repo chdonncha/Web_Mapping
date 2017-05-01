a980f9ba6842233feff69040b61c8ca6548b4016
donncha <chdonncha@gmail.com>
2017-05-01 13:00:22 +0100

    removed redundent code

--- wmap2017-cordova-master/www/js/index.js
+++ wmap2017-cordova-master/www/js/index.js
@@ -236,32 +236,6 @@ function myGeoPosition(p) {
     this.timestamp = (p.timestamp) ? p.timestamp : new Date().getTime();
 }
 
-// function updateTest() {
-//     $.ajax({
-//             type: "GET",
-//             headers: {"Authorization": localStorage.authtoken},
-//             url: HOST + URLS["userme"]
-//         }).done(function (data, status, xhr) {
-//             $(".sp-username").html(xhr.responseJSON.properties.username);
-//         }).fail(function (xhr, status, error) {
-//             $(".sp-username").html("");
-//         });
-// }
-
-// function setUserName() {
-//     console.log("In setUserName.");
-//     $.ajax({
-//         type: "PATCH",
-//         headers: {"Authorization": localStorage.authtoken},
-//         url: HOST + URLS["userme"],
-//         data: JSON.stringify({first_name: "tester123"})
-//     }).done(function (data, status, xhr) {
-//         showOkAlert("success")
-//     }).fail(function (xhr, status, error) {
-//         showOkAlert("failure")
-//     });
-// }
-
 function setUserName() {
     console.log("Display Username = " + $(".sp-username"));
     $(".sp-username").html(localStorage.lastUserName);
