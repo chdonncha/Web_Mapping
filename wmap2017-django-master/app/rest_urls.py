1022c986a3ab28f21a94546fad101834d0e47408
donncha <chdonncha@gmail.com>
2017-05-02 23:06:40 +0100

    fixed username endpoint

--- wmap2017-django-master/app/rest_urls.py
+++ wmap2017-django-master/app/rest_urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^tokenlogin/$', rest_views.token_login, name='token-login'),
     url(r'^userme/$', rest_views.UserMe_R.as_view(), name='user-me'),
     url(r'^users/$', rest_views.UsersList.as_view(), name='users'),
-    url(r'^user/(?P<username>[a-zA-Z0-9_.+-]+)/$', rest_views.UserOther_R.as_view(), name='user-username'),
-    url(r'^user/(?P<uid>\d+)/$', rest_views.UserOther_R.as_view(), name='user-id'),
+    url(r'^user/(?P<username>[a-zA-Z0-9_.+-]+)/$', rest_views.UserOther_R.as_view(), name='user-name'),
+    url(r'^user/(?P<uid>\d+)/$', rest_views.UserOther_R.as_view(), name='user-username'),
     url(r'^updateposition/$', rest_views.UpdatePosition.as_view(), name='update-position'),
 ]
